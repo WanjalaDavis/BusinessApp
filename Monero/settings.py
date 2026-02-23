2529339b1c0d85a945c92135aae440b2e0488ae9
WanjalaDavis <wanjaladevis81@gmail.com>
2026-02-23 14:18:13 +0300

    Made a few changes

--- Monero/settings.py
+++ Monero/settings.py
@@ -86,23 +86,23 @@ SESSION_COOKIE_SAMESITE = 'None'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'monero_db',       
-        'USER': 'root',            
-        'PASSWORD': '',            
-        'HOST': '127.0.0.1',       
-        'PORT': '3306',            
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'monero_db',       
+#         'USER': 'root',            
+#         'PASSWORD': '',            
+#         'HOST': '127.0.0.1',       
+#         'PORT': '3306',            
+#     }
+# }
 
 
 
